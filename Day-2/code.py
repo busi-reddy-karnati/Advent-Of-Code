099cdfa73dfc3f5e89e88b2dba775eda1f02428f
busi-reddy-karnati <bk2444@nyu.edu>
2023-12-12 16:36:39 -0500

    Day-2-Part2

--- Day-2/code.py
+++ Day-2/code.py
@@ -1,22 +1,31 @@
-allowed = {
-    "blue":14,
-    "green":13,
-    "red":12
-}
+
 def possible(draw):
+    required = {
+        "blue": 0,
+        "green": 0,
+        "red": 0
+    }
     ball_list = draw.split(",")
     for balls in ball_list:
         ball_ct, ball_color = balls.strip().split(" ")
-        if int(ball_ct) > allowed[ball_color]:
-            return False
-    return True
+        required[ball_color] = max(required[ball_color], int(ball_ct))
+    return required
 def process(s):
     game_id, draws = s.split(":")
     draws = draws.split(";")
+    required = {
+        "blue": 0,
+        "green": 0,
+        "red": 0
+    }
     for draw in draws:
-        if not possible(draw):
-            return 0
-    return int(game_id.split(" ")[1])
+        reqd_temp = possible(draw)
+        for k in required:
+            required[k] = max(required[k], reqd_temp[k])
+    pro = 1
+    for k in required:
+        pro *= required[k]
+    return pro
 ans = 0
 with open('input.txt') as file:
     for line in file:
